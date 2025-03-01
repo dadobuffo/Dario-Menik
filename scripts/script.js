508a944208631a184d5453a632c747b6f1afa577
dadobuffo <dariomenik@gmail.com>
2025-03-01 19:49:10 +0100

    new header

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,19 @@ function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+
+function updateHeaderVisibility() {
+  let scrollThreshold = window.matchMedia("(max-width: 800px)").matches
+    ? 31 * 16
+    : 51 * 16;
+
+  if (window.scrollY > scrollThreshold) {
+    document.querySelector(".fixed-header").style.top = "0";
+  } else {
+    document.querySelector(".fixed-header").style.top = "-4rem";
+  }
+}
+
+window.addEventListener("scroll", updateHeaderVisibility);
+
+updateHeaderVisibility();
