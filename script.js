5f721128cfcf3bf7af729fa7aabb9116051ecd6f
dadobuffo <dariomenik@gmail.com>
2025-06-11 15:48:28 +0200

    add temp overlay and fix contact

--- script.js
+++ script.js
@@ -203,3 +203,19 @@ const observer = new IntersectionObserver(callback, options);
 videos.forEach((video) => {
   observer.observe(video);
 });
+
+// temporary modal
+
+window.onload = function () {
+  document.getElementById("temp-modal").style.display = "flex";
+};
+
+function closeModal() {
+  document.getElementById("temp-modal").style.display = "none";
+}
+
+document.getElementById("temp-modal").addEventListener("click", function (e) {
+  if (e.target === this) {
+    closeModal();
+  }
+});
